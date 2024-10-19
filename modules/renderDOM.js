92a9ac59b751660dbd087ae714df0fbb3ddecebf
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-10-19 14:42:17 +1100

    Get weather icons and create function to display

--- modules/renderDOM.js
+++ modules/renderDOM.js
@@ -6,6 +6,7 @@ const currentCardMinTemp = document.querySelector('#curMinTemp');
 const currentCardMaxTemp = document.querySelector('#curMaxTemp');
 const currentCardUVIndex = document.querySelector('#curUVIndex');
 const currentCardChanceOfRain = document.querySelector('#curChanceOfRain');
+const currentCardMainIcon = document.querySelector('.curMainIcon');
 
 export function revealElement(element) {
   element.style.display = 'flex';
@@ -13,6 +14,48 @@ export function revealElement(element) {
 
 export function hideElement(element) {
   element.style.display = 'none';
+}
+
+function setWeatherIcon(condition) {
+  if (condition === 'clear-day' || condition === 'clear-night') {
+    currentCardMainIcon.style.webkitMask =
+      'url(./icons/sun-solid.svg) no-repeat center';
+    currentCardMainIcon.style.mask =
+      'url(./icons/sun-solid.svg) no-repeat center';
+  }
+  if (condition === 'fog' || condition === 'cloudy') {
+    currentCardMainIcon.style.webkitMask =
+      'url(./icons/cloud-solid.svg) no-repeat center';
+    currentCardMainIcon.style.mask =
+      'url(./icons/cloud-solid.svg) no-repeat center';
+  }
+  if (
+    condition === 'partly-cloudy-day' ||
+    condition === 'partly-cloudy-night'
+  ) {
+    currentCardMainIcon.style.webkitMask =
+      'url(./icons/cloud-sun-solid.svg) no-repeat center';
+    currentCardMainIcon.style.mask =
+      'url(./icons/cloud-sun-solid.svg) no-repeat center';
+  }
+  if (condition === 'snow') {
+    currentCardMainIcon.style.webkitMask =
+      'url(./icons/snowflake-solid.svg) no-repeat center';
+    currentCardMainIcon.style.mask =
+      'url(./icons/snowflake-solid.svg) no-repeat center';
+  }
+  if (condition === 'wind') {
+    currentCardMainIcon.style.webkitMask =
+      'url(./icons/wind-solid.svg) no-repeat center';
+    currentCardMainIcon.style.mask =
+      'url(./icons/wind-solid.svg) no-repeat center';
+  }
+  if (condition === 'rain') {
+    currentCardMainIcon.style.webkitMask =
+      'url(./icons/cloud-rain-solid.svg) no-repeat center';
+    currentCardMainIcon.style.mask =
+      'url(./icons/cloud-rain-solid.svg) no-repeat center';
+  }
 }
 
 export function populateCurrentWeatherCard(data, unit) {
@@ -24,4 +67,5 @@ export function populateCurrentWeatherCard(data, unit) {
   currentCardMaxTemp.textContent = `Max ${data.maxtempCelcius}`;
   currentCardUVIndex.textContent = `UV Index ${data.curUV}`;
   currentCardChanceOfRain.textContent = `Chance of rain: ${data.chanceOfRain}`;
+  setWeatherIcon(data.icon);
 }
