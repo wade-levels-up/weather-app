fae96095079952b4d4081474d2073654685571ce
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-10-19 11:19:36 +1100

    Add weather data for week on form submission

--- src/index.js
+++ src/index.js
@@ -8,16 +8,21 @@ const form = document.querySelector('#main-form');
 const inputCity = document.querySelector('#input-city');
 
 let todaysWeather;
-// let weeksWeather = await returnWeeksWeatherObjects('melbourne');
-// console.table(todaysWeather);
-// console.log(weeksWeather);
+let weeksWeather;
 
 async function setTodaysWeather(city) {
   todaysWeather = await returnCurrentWeatherObject(city);
   console.table(todaysWeather);
 }
 
+async function setWeeksWeather(city) {
+  weeksWeather = await returnWeeksWeatherObjects(city);
+  console.table(weeksWeather);
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  console.log('retrieving data...');
   setTodaysWeather(`${inputCity.value}`);
+  setWeeksWeather(`${inputCity.value}`);
 });
